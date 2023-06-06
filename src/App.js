9b51d2756e9a7e51a939008753e1ae9273795645
Jeremy Jeremiah <jeremyjeremiah587@gmail.com>
2023-06-05 18:39:34 -0600

    Week 1 Workshop Assignment task3 part1 complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+//import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import { Container,Navbar,NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+import Header from './components/Header';
+
 //import CampsiteCard from './features/campsites/CampsiteCard';
 import CampsitesList from './features/campsites/CampsitesList';
 //import {CAMPSITES} from './app/shared/CAMPSITES';
@@ -11,13 +11,7 @@ import CampsitesList from './features/campsites/CampsitesList';
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
-          <Container>
-            <NavbarBrand href='/'>
-              <img src={NucampLogo} alt='nucamp logo' />
-            </NavbarBrand>
-          </Container>
-      </Navbar>
+      <Header />
       <CampsitesList />
     </div>
   );
