66f7aad457812bc9e0e622b2ce8415009cade549
Jeremy Jeremiah <jeremyjeremiah587@gmail.com>
2023-06-08 10:08:15 -0600

    Part 1: useState() with a random campsite

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -2,18 +2,20 @@ import { Container,Row,Col,Button } from "reactstrap";
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CampsitesList from "../features/campsites/CampsitesList";
 import { selectRandomCampsite } from "../features/campsites/campsitesSlice";
+import { useState } from "react";
 
 const CampsiteDirectoryPage = () => {
-    let selectedCampsite = selectRandomCampsite();
+    // let selectedCampsite = selectRandomCampsite();
 
-    const toggleCampsite = () => {
-        selectedCampsite = selectRandomCampsite();
-        console.log(selectedCampsite);
-    }
+    // const toggleCampsite = () => {
+    //     selectedCampsite = selectRandomCampsite();
+    //     console.log(selectedCampsite);
+    // }
+    const [selectedCampsite, toggleCampsite ] = useState(selectRandomCampsite())
 
     return (
         <Container>
-            <Button onClick={(toggleCampsite)}>
+            <Button onClick={() => toggleCampsite(selectRandomCampsite())}>
                 Select Random Campsite
             </Button>
             <Row>
